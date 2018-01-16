521e93dcbb55c96689e5cafec0a194f57f995a27
Andrew McDowell <andrew@propelleraero.com>
2018-01-16 15:09:36 +1100

    running in not headless mode

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const puppeteer = require("puppeteer");
 const HOMEPAGE = "https://www.propelleraero.com";
 
 (async () => {
-    // launch puppeteer
-    const browser = await puppeteer.launch();
+    // launch puppeteer ({headless: true} runs without opening chrome)
+    const browser = await puppeteer.launch({ headless: false });
     // create a new page
     const page = await browser.newPage();
     // navigate to the homepage
     await page.goto(HOMEPAGE);
+
+    // do your scraping here
+
+    // close the browser
+    browser.close();
 })();
